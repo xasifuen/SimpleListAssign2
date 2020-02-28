aa7cb5419238b3bdf75e4d2d3a9616b6a6434f7d
Xavi <Xavi@64.189.119.236>
2020-02-27 22:00:00 -0700

    Updated add and remove methods, added append, first, last, size methods.

--- cse360assign2/src/cse360assign2/SimpleList.java
+++ cse360assign2/src/cse360assign2/SimpleList.java
@@ -15,7 +15,7 @@ public class SimpleList
 	 */
 	private int count = 0;
 	private int[] listArray = new int[10];
-	
+
 	public SimpleList()
 	{
 		listArray = new int[10];
@@ -24,18 +24,34 @@ public class SimpleList
 	public void add(int num)
 	{
 		int[] temp = new int[listArray.length];
-		for(int tick = 0; tick < listArray.length; tick++)
+		if(listArray[listArray.length - 1] == 0)
 		{
-			temp[tick] = listArray[tick];
-		}
-		listArray[0] = num;
-		for(int tick = 1; tick < listArray.length - 1; tick++)
-		{
+			for(int tick = 0; tick < listArray.length; tick++)
+			{
+				temp[tick] = listArray[tick];
+			}
+			listArray[0] = num;
+			for(int tick = 1; tick < listArray.length - 1; tick++)
+			{
 			
-			listArray[tick] = temp[tick - 1];
+				listArray[tick] = temp[tick - 1];
 
+			}
 		}
-		
+		else
+		{
+			int[] largerArray = new int[(int)(1.5*listArray.length)];
+			int[] tempArray = new int[(int)(1.5 * listArray.length)];
+			for(int tick = 0; tick < listArray.length; tick++)
+			{
+				tempArray[tick] = listArray[tick];
+			}
+			listArray[0] = num;
+			for(int tick = 1; tick < largerArray.length - 1; tick++)
+			{
+				largerArray[tick] = tempArray[tick - 1];
+			}
+		}
 		//listArray[0] = num;
 		count++;
 	}
@@ -43,13 +59,6 @@ public class SimpleList
 	
 	public void remove(int num)
 	{
-		/*
-		 * for(int tick = 0; tick < listArray.length; tick++) { if(listArray[tick] ==
-		 * num) { while(tick < listArray.length) { listArray[tick] = listArray[tick +
-		 * 1];
-		 * 
-		 * } count--; } }
-		 */
 		int[] temp = new int[listArray.length];
 		int mark = 0;
 
@@ -64,6 +73,15 @@ public class SimpleList
 		for(int tick = mark; tick < listArray.length - 1; tick++)
 		{
 			listArray[tick] = listArray[tick + 1];
+		}
+		if((listArray.length - count) > (int)(.25*listArray.length))
+		{
+			int[] shorterArray = new int[listArray.length - 1];
+			for(int tick = 0; tick < shorterArray.length; tick++)
+			{
+				shorterArray[tick] = listArray[tick];
+			}
+
 		}
 		count--;
 	}
@@ -91,5 +109,52 @@ public class SimpleList
 		return -1;
 	}
 	
-	
+	public void append (int num)
+
+	{
+		int[] temp = new int[listArray.length];
+		if(listArray[listArray.length - 1] == 0)
+		{
+			listArray[listArray.length - 1] = num;
+		}
+		else
+		{
+			int[] largerArray = new int[(int)(1.5*listArray.length)];
+			//int[] tempArray = new int[(int)(1.5 * listArray.length)];
+			for(int tick = 0; tick < listArray.length; tick++)
+			{
+				largerArray[tick] = listArray[tick];
+			}
+			largerArray[largerArray.length - 1] = num;
+		}
+	//listArray[0] = num;
+	count++;
+	}
+
+	public int first()
+	{
+		if(listArray[0] == 0)
+		{
+			return -1;
+		}
+		else
+		{
+			return listArray[0];
+		}
+			
+	}
+
+	public int last()
+	{
+		if(listArray[listArray.length - 1] != 0)
+			return listArray[listArray.length - 1];
+		else
+			return -1;
+	}
+
+	public int size()
+	{
+		return listArray.length;
+	}
 }
+
